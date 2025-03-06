f76f311c1ee1c776b355e694041c5f3972d5b1d8
mapa5 <90824681+mapa5@users.noreply.github.com>
2025-03-06 14:05:39 +0100

    change all html file to root

--- script/pokemon.js
+++ script/pokemon.js
@@ -2,10 +2,10 @@ let params = new URLSearchParams(document.location.search);
 let id = params.get("id");
 
 if (id != null) {
-    async(id)=> {
-    create_vignette(".content", id);
-    let data1 = await call_poke_gen(id);
-    let data2 = await get_poke_info(id);
-    fill_vignette(data1, data2, id);
-    }
-};
+    (async (id) => {
+        create_vignette(".content", id);
+        let data1 = await call_poke_gen(id);
+        let data2 = await get_poke_info(id);
+        fill_vignette(data1, data2, id);
+    })(id);
+}
